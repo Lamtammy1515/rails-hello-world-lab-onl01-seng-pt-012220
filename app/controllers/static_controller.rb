abc16f753dc429e36c635b3c4785b20143cdf73d
Tammy Lam <lamtammy1515@gmail.com>
2020-05-24 16:07:50 +0000

    Automatically backed up by Learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,9 @@
 class StaticController < ApplicationController
    def hello_world
-     render "index"
+     render "index.html"
+   end 
+   
+   get '/hello_world' do 
+     erb:'index.html'
    end 
 end
